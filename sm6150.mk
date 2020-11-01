ec736a5456020fb1ea53816f7d4a87e1a6679cf2
Arian <arian.kulmer@web.de>
2020-11-01 21:28:07 +0100

    sm6150-common: Don't append -qti to power HAL name
    Change-Id: Icaa436d9961bb7ba0e560b3d03da73752f1e15a0

--- sm6150.mk
+++ sm6150.mk
@@ -277,7 +277,7 @@ TARGET_BOARD_PLATFORM := $(MSMSTEPPE)
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 # QTI
 PRODUCT_COPY_FILES += \
