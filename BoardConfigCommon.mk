91c74f2a64da2f50dc9eecca4cf6f51ca29f16c7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-16 16:07:54 +0530

    sm6150-common: Adapt DT2W extension to AOSPA power HAL
    https://github.com/pa-gr/android_vendor_aospa/commit/027c4d9fea9bce9df0b8de6314e03934d97f8652

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,7 +103,7 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 TARGET_DISABLED_UBWC := true
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+TARGET_POWER_FEATURE_EXT_LIB := //$(COMMON_PATH):libpowerfeature_ext_sm6150
 
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
