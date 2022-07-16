741fea6d84b7b472a2912472fe9a21ff554c8b8e
anupam41 <anupamsamanta41@gmail.com>
2022-07-16 08:07:34 +0200

    Revert "sm6150-common: Migrate to Xiaomi power HAL AIDL"
    This reverts commit 7e0b1d8983fef3127c9c251d7e93cf762962ef01.

--- sm6150.mk
+++ sm6150.mk
@@ -268,7 +268,7 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service-qti
 
 # QTI
 PRODUCT_PACKAGES += \
@@ -310,8 +310,6 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/xiaomi
 
 # Telephony
