404b1dfda46e7a93e33c5df7a59cf428f20c2c35
anupam41 <anupamsamanta41@gmail.com>
2022-07-16 08:04:59 +0200

    sm6150-common: remove lineage specific overlay

--- sm6150.mk
+++ sm6150.mk
@@ -262,8 +262,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
