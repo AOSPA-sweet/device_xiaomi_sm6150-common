f49e907bc50850bc41a7b00c27878d3f763a2009
anupam41 <anupamsamanta41@gmail.com>
2022-08-28 11:25:27 +0530

    sm6150-common:fix use common QTI wlan component [1/2]

--- sm6150.mk
+++ sm6150.mk
@@ -317,8 +317,12 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 # WiFi
-PRODUCT_PACKAGES += \
     WifiOverlay
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
 PRODUCT_PACKAGES += \
